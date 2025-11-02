1bd03ee8b3d682d701cb1fe1a23297d4f62aab1a
John Sosoka <john@sosoka.com>
2025-11-01 21:23:23 -0600

    feat: add apex domain (neuroboy.com) infrastructure

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
 // Bucket names for deployment
+output "root_bucket" {
+  description = "S3 bucket for neuroboy.com"
+  value       = "neuroboy.com"
+}
+
 output "www_bucket" {
   description = "S3 bucket for www.neuroboy.com"
   value       = "www.neuroboy.com"
--- terraform/website.tf
+++ terraform/website.tf
@@ -1,3 +1,12 @@
+// neuroboy.com - Apex domain
+module "root" {
+  source = "git::https://github.com/johnsosoka/jscom-tf-modules.git//modules/static-website?ref=main"
+
+  domain_name  = local.domain_name
+  root_zone_id = local.zone_id
+  acm_cert_id  = local.cert_arn
+}
+
 // www.neuroboy.com - Main website
 module "www" {
   source = "git::https://github.com/johnsosoka/jscom-tf-modules.git//modules/static-website?ref=main"
